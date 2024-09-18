90cfd68a45a6cde71b2ee672120095e66dd2cd79
James Orson <jamesaorson@gmail.com>
2024-09-18 13:07:27 -0700

    Remove scheme lange server

--- vim/config/after/plugin/autocmd.lua
+++ vim/config/after/plugin/autocmd.lua
@@ -8,21 +8,3 @@ vim.api.nvim_create_autocmd("BufNewFile", {
   command = "setfiletype scheme",
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'scheme',
-  callback = function(args)
-    vim.lsp.start({
-      name = 'scheme-langserver',
-      cmd = {
-		'{path-to-run}',
-        '~/scheme-langserver.log',
-        --enable multi-thread
-        'enable',
-        --disable type inference, because it's on very early stage.
-        'diable',
-      },
-      root_dir = vim.fs.root(args.buf, {'.gitignore','AKKU.manifest'}),
-    })
-  end
-})
-
--- vim/config/after/plugin/lsp.lua
+++ vim/config/after/plugin/lsp.lua
@@ -2,6 +2,7 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 require'lspconfig'.scheme_langserver.setup{
+	cmd = { "scheme-langserver.sps" },
 	filetypes = { "scheme", "scm", "sls" },
 }
 
