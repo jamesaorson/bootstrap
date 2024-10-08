d8d3d8bb05ce4dfb80ada586ea21f96b46edb7c0
James Orson <jamesaorson@gmail.com>
2024-10-08 10:05:53 -0700

    Convert tabs to spaces. Change Emacs git source.

--- vim/config/lua/bootstrap/set.lua
+++ vim/config/lua/bootstrap/set.lua
@@ -24,7 +24,7 @@ common.run("indent", function()
 	vim.opt.tabstop = 4
 	vim.opt.softtabstop = 4
 	vim.opt.shiftwidth = 4
-	vim.opt.expandtab = false
+	vim.opt.expandtab = true
 	vim.opt.smartindent = true
 	vim.opt.wrap = false
 end)
